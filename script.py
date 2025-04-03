a0beaa771ed8b58cd276fb9f23b879abf95977dd
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-04-02 22:13:11 -0400

    # this is my first class called Home. class Home:
        price_objs = 0.15
    
        def __init__(self, appliance, room, device, total_cost):
            self.appliance = appliance
            self.room = room
            self.device = device
            self.total_cost = total_cost
    
        def home_price(self):
            return '{} is debited from {}'.format(self.appliance, self.total_cost)
    
        def rate_of_price(self):
            return self.total_cost * Home.price_objs
    
        def under_rate(self):
            if self.total_cost <= 500:
                return True
            else:
                return "{} needs to be remove".format(self.appliance)
    
        @classmethod
        def home_rate(cls, amount):
            cls.price_objs = amount
    Home.home_rate(0.18)
    home1 = Home('sofa', 'bathroom', 'cell', 1000)
    home2 = Home('table', 'bedroom', 'cam', 5000)
    print(home1.rate_of_price())

--- script.py
+++ script.py
@@ -1,17 +1,73 @@
-class Home:
+class Employee:
+    num_employee = 0
+    annual_rate = 0.05
 
-    def __int__(self, appliance, room, device):  # Corrected __int__ to __init__
-        self.appliance = appliance
-        self.room = room
-        self.device = device
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@carlos.com'
 
-    def __str__(self):  # Optional: To print a friendly string representation of the object
-        return f'Home(appliance={self.appliance}, room={self.room}, device={self.device})'
+        Employee.num_employee += 1
 
-home_1 = Home('sofa', 'bathroom', 'cell')
+    def full_name(self):
+        return '{} {}'.format(self.first, self.last)
 
-print(home_1)
+    def pay_rate(self):
+        return Employee.annual_rate * self.pay
 
+
+    @classmethod
+    def rate_new_pay(cls, amount):
+        cls.annual_rate = amount
+
+
+    @classmethod
+    def from_name(cls, emp_new):
+        first, last, pay = emp_new.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+class Developers(Employee):
+
+    def __init__(self,first, last, pay, lenguage):
+        super().__init__(first, last, pay)
+        self.lenguage = lenguage
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.full_name())
+
+
+emp4 = Developers('carlos', 'javier', 25000, 'python')
+emp5 = Developers('Enna', 'Castillo', 28000, 'java')
+
+
+emp1 = Manager('carlos', 'javier', 25000, [emp4])
+emp2 = Manager('Enna', 'Castillo', 28000, [emp5])
 
 
 
