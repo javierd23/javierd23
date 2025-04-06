18d4a09329e4a67fa9af4bf78062a1901f8c31d3
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-04-06 14:30:56 -0400

    Learn Built-in functions with example.

--- CarlosP01/Builtin.py
+++ CarlosP01/Builtin.py
@@ -1,1 +1,31 @@
-age =  10
+# abs() function. This return the absolute value of a number.
+
+age = -18
+print(abs(age)) #The result will be 18.
+
+def numbers(number): #othe way to do it def numbers(number). And add the 5
+    if number is not abs(number):
+        return False
+    return True
+
+result = numbers(5)
+print(result)
+
+#this is repr(). This return a string representation of an object one that's meant to be unambiguius and usable for debugging or development.
+
+name = 'Carlos'
+print(repr(name))
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"Dog('{self.name}')"
+
+my_dog = Dog('Brandon')
+
+print(repr(my_dog))
+
+
+
