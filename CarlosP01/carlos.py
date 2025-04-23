e4297a77ee761532f830fb4dcb8fe62a29081198
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-04-22 22:37:41 -0400

    Pay_rate

--- CarlosP01/carlos.py
+++ CarlosP01/carlos.py
@@ -1,87 +1,28 @@
+#this is a pay rate calculated by amount of the hours * by the rate and after 40 it will add %1.5 to the hrs.
 
-# this is my first class called Employees.
-class Employee:
-    num_employee = 0
-    annual_rate = 0.05
+def computepay():
+    h = input('enter a number')
+    r = input('enter a number')
+    hs = float(h)
+    rs = float(r)
 
-#intances.
+    if hs > 40:
+        ehs = (hs - 40)
+        rht = (hs - ehs) * rs
+        pr =   ((ehs * rs) * 1.5)
+        prt = rht + pr
+        return prt
+    else:
+        return hs * rs
 
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@carlos.com'
-
-        Employee.num_employee += 1
-
-#first functions to add Full Name.
-
-    def full_name(self):
-        return '{} {}'.format(self.first, self.last)
-
-#Function to add annual raise pay.
-
-    def pay_rate(self):
-        return Employee.annual_rate * self.pay
-
-#This is a ClassMethod to handle the rate for the raise's annual pay
-
-    @classmethod
-    def rate_new_pay(cls, amount):
-        cls.annual_rate = amount
-
-#This is a ClassMethod to split user with dashes input.
-
-    @classmethod
-    def from_name(cls, emp_new):
-        first, last, pay = emp_new.split('-')
-        return cls(first, last, pay)
-
-#This is to confer if the day is a week day.
-
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
-
-#this is a second Class associated with the main Class, to add a new type of employees.
-
-class Developers(Employee):
-
-    def __init__(self,first, last, pay, lenguage):
-        super().__init__(first, last, pay)
-        self.lenguage = lenguage
-
-#this is a third Class associated with the main Class, to add a new type of employees.
-#here we created functions to add, remove and print from this Class.
-
-class Manager(Employee):
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
-
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
-
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.full_name())
+result = computepay()
+print('Pay rate', result)
 
 
-emp4 = Developers('carlos', 'javier', 25000, 'python')
-emp5 = Developers('Enna', 'Castillo', 28000, 'java')
 
 
-emp1 = Manager('carlos', 'javier', 25000, [emp4])
-emp2 = Manager('Enna', 'Castillo', 28000, [emp5])
 
-#End...
+
+
+
+
